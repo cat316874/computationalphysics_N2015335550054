50a59923f7a0a1e676593a684b773f01aca2d2a1
cat316874 <1819476594@qq.com>
2016-09-25 15:52:51 +0800

    Add files via upload

---
+++ Exercise 03 L01.py
@@ -0,0 +1,30 @@
+import os
+A = " #       # ######     # #        #        ##       #"
+B = "  #     #  #                    # #       # #      #"
+C = "   #   #   #          # #      #   #      #  #     #"
+D = "    # #    #          # #     #     #     #   #    #"
+E = "     #     #######    # #    #########    #    #   #" 
+F = "     #     #          # #   #         #   #     #  #"
+G = "     #     #        # # #  #           #  #      # #" 
+I = "     #     ########  ## # #             # #       ##"
+space=" "
+while len(A)<168:
+    i=os.system('cls') 
+    print A
+    print B
+    print C
+    print D
+    print E
+    print F
+    print G
+    print I
+    A = space + A
+    B = space + B
+    C = space + C
+    D = space + D
+    E = space + E
+    F = space + F
+    G = space + G
+    I = space + I
+raw_input()
+      
